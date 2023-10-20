92db630d1932d1eecda864ca900d276f5971118b
Zachary Elbring <elbringzach@gmail.com>
2023-10-20 16:42:12 -0500

    fixed generateQuestions function and added some css styling

--- script.js
+++ script.js
@@ -2,44 +2,49 @@
 var startButton = document.querySelector('.button');
 var timerElement = document.querySelector('.timer-count');
 var questionsEl = document.querySelector('.questions');
-
+var indexQuestion = 0;
+var choiceA = document.getElementById('A');
+var choiceB = document.getElementById('B');
+var choiceC = document.getElementById('C');
 let quizQuestions = [
     {
         question: 'What HTML element contains Javascript?',
-        choiceA: '',
-        choiceB: '',
-        choiceC: '',
+        choiceA: '<j>',
+        choiceB: '<script>',
+        choiceC: '<javascript>',
         answer: 'B'
     },
     {
-        question: '',
-        choiceA: '',
-        choiceB: '',
-        choiceC: '',
+        question: 'To make a function work you must first ___ the function.',
+        choiceA: 'call',
+        choiceB: 'start',
+        choiceC: 'justify',
         answer: 'A'
     },
     {
-        question: '',
-        choiceA: '',
-        choiceB: '',
-        choiceC: '',
+        question: 'True and false are called ___ values.',
+        choiceA: 'real',
+        choiceB: 'positive and negative',
+        choiceC: 'boolean',
         answer: 'C'
     },
     {
-        question: '',
-        choiceA: '',
-        choiceB: '',
-        choiceC: '',
+        question: 'What do you call text wrapped in quotes?',
+        choiceA: 'A string',
+        choiceB: 'A value',
+        choiceC: 'An array',
         answer: 'A'
     },
     {
-        question: '',
-        choiceA: '',
-        choiceB: '',
-        choiceC: '',
+        question: 'What method is used to get a random value in Javascript?',
+        choiceA: 'scramble()',
+        choiceB: 'math.random()',
+        choiceC: 'generate-random',
         answer: 'B'
     },
 ];
+
+var lastIndexQuestion = quizQuestions.length - 1;
 
 var timer;
 var timerCount;
@@ -48,6 +53,7 @@ function startGame() {
 timerCount = 75;
 startButton.disabled = true;
 startTimer()
+generateQuestions(console.log)
 }
 
 
@@ -62,8 +68,12 @@ function startTimer() {
     }, 1000);
 }
 
-function showQuestions() {
-
+function generateQuestions() {
+let q = quizQuestions[indexQuestion];
+questionsEl.innerHTML = '<h2>' + q.question + '<h2>';
+choiceA.textContent = q.choiceA;
+choiceB.textContent = q.choiceB;
+choiceC.textContent = q.choiceC;
 }
 
 startButton.addEventListener('click', startGame);
